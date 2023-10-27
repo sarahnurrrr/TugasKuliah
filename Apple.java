f276a1918bf5e330a5b78a7f7fed12ae48618c68
sarahnurrrr <95034627+sarahnurrrr@users.noreply.github.com>
2023-10-27 17:47:31 +0700

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,9 @@
+package BillLaptopUTS;
+public class Apple {
+
+    String nama = "Apple Mac Book Air";
+    double harga = 10462100;
+    double diskon = 0;
+
+
+}
---
+++ Fujitsu.java
@@ -0,0 +1,7 @@
+package BillLaptopUTS;
+
+public class Fujitsu {
+    String nama = "Fujitsu Lifebook LH531V";
+    double harga = 6397600;
+    double diskon = 0;
+}
