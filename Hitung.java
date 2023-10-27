433cb172d05edce04e955102dbd188dbf0fb8b26
sarahnurrrr <95034627+sarahnurrrr@users.noreply.github.com>
2023-10-27 17:36:38 +0700

    Add files via upload

---
+++ Hitung.java
@@ -0,0 +1,39 @@
+package Kalkulator;
+
+public class Hitung {
+    public int tambah (int nilai1, int nilai2){
+        int hasil = nilai1 + nilai2;
+        return hasil;
+    }
+    public double tambah (double nilai3, double nilai4){
+        double hasil = nilai3 + nilai4;
+        return hasil;
+    }
+
+    public int kurang (int nilai1, int nilai2){
+        int hasil = nilai1 - nilai2;
+        return hasil;
+    }
+    public double kurang (double nilai3, double nilai4){
+        double hasil = nilai3 - nilai4;
+        return hasil;
+    }
+
+    public int kali (int nilai1, int nilai2){
+        int hasil = nilai1 * nilai2;
+        return hasil;
+    }
+    public double kali (double nilai3, double nilai4){
+        double hasil = nilai3 * nilai4;
+        return hasil;
+    }
+
+    public int bagi (int nilai1, int nilai2){
+        int hasil = nilai1 / nilai2;
+        return hasil;
+    }
+    public double bagi (double nilai3, double nilai4){
+        double hasil = nilai3 / nilai4;
+        return hasil;
+    }
+}
